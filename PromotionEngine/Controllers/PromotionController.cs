dcfe4df7b7156166e14fc7830d2e5a5b2a06a08b
Vinay H C Reddy <vinay.reddy@siemens.com>
2021-06-05 09:11:32 +0530

    Check github action tirggered

--- PromotionEngine/Controllers/PromotionController.cs
+++ PromotionEngine/Controllers/PromotionController.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Promotion.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PromotionEngine.Controllers
 {
